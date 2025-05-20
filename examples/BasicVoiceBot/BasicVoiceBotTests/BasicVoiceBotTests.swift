1c7c71602454f67dae3f01b7ee49c9fad8fa2ee5
janak2 <janakagrawal1309@gmail.com>
2025-05-19 17:29:19 -0700

    add example

---
+++ examples/BasicVoiceBot/BasicVoiceBotTests/BasicVoiceBotTests.swift
@@ -0,0 +1,17 @@
+//
+//  BasicVoiceBotTests.swift
+//  BasicVoiceBotTests
+//
+//  Created by Janak Agrawal on 5/19/25.
+//
+
+import Testing
+@testable import BasicVoiceBot
+
+struct BasicVoiceBotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
